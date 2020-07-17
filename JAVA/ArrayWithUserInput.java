ad25f4402b552f6cc22d93d848ce1398956cebff
Praveen Kumar Panjugula <Praveen@users-MacBook-Pro-2.local>
2020-07-17 01:44:23 -0400

    test

---
+++ JAVA/ArrayWithUserInput.java
@@ -0,0 +1,17 @@
+/**
+ * ArrayWithUserInput
+ */
+public class ArrayWithUserInput {
+
+    public static void main(String[] args) {
+        // scanner ArrayInput_1= new scanner(System.in);
+        // system.out.println("Enter the values into array_1:");
+        // scanner ArrayInput_2= new scanner(System.in);
+        // system.out.println("Enter the values into array_2:");
+        System.out.println("HelloWorld_test");
+        System.out.println("HelloWorld");
+        System.out.println("Omnamonarayanaya");
+
+    }
+
+}
